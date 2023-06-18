dd86e6f6f09ec63c430a989f2572ee1da00b70c0
Brian Kimbrow <bkimbrow25@gmail.com>
2023-06-17 22:26:46 -0500

    randor update

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-'use script';
+"use script";
+
+let chart = document.getElementById('chart').getContext('2d');
+
+let chart1 = new Chart(chart, {
+  type: 'bar',
+  data: {
+    labels: ['Bass", "Catfish", "Trout", "Blue Gill'],
+    datasets: [
+      {
+        label: 'Fish Population',
+        data: [22000, 18000, 12200, 10000],
+      },
+    ],
+  },
+  options: {},
+});
+console.log(Chart);
