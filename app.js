90a610b9e4fac385c19e31f15202d96381d07712
Brian Kimbrow <bkimbrow25@gmail.com>
2023-06-18 00:28:43 -0500

    radnor  update

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 "use script";
 
-// Brian's Radnor lake chart code Begining 
-let chart = document.getElementById('chart-canvas').getContext('2d');
+// Brian's Radnor lake chart code Begining
+let chart = document.getElementById("chart-canvas").getContext("2d");
 
 let chart1 = new Chart(chart, {
-  type: 'doughnut',
+  type: "doughnut",
   data: {
     labels: ['Bass", "Catfish", "Trout", "Blue Gill'],
     datasets: [
       {
-        label: 'Fish Population',
+        label: "Fish Population",
         data: [22000, 18000, 12200, 10000],
       },
     ],
   },
   options: {},
 });
-console.log(chart);
 
 // Where i stopped working on  Next person start five lines below me!
