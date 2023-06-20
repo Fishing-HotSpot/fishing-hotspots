c2714b913f4c9840d8ee51c9f63ce82150c7200e
Ajamu Page <kil91402@gmail.com>
2023-06-20 16:42:00 -0500

    Started adding javascript

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-'use script';
+"use script";
+
+let chart;
+let ctx = document.getElementById("canvas-chart-b");
+let dataObj = {
+  type: "doughnut",
+  data: {
+    labels: [
+      "Largemouth Bass",
+      "Crappie",
+      "Striped Bass",
+      "Sauger",
+      "Channel Catfish",
+    ],
+    datasets: [
+      {
+        label: "Fish Population",
+        data: [],
+      },
+    ],
+  },
+};
